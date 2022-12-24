112ca02e65e9937c93d605c27172169299f206c5
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2022-12-24 20:47:07 -0300

    feat(fields): add RichTextField

---
+++ src/components/fields/RichTextField/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { Labeled, RichTextField as RaRichTextField, RichTextFieldProps } from 'react-admin'
+import { LabeledFieldProps } from '../../../config'
+
+export function RichTextField(props: LabeledFieldProps<RichTextFieldProps>) {
+  const { useLabel, ...rest } = props
+  const field = <RaRichTextField {...rest} />
+  return useLabel ? <Labeled>{field}</Labeled> : <>{field}</>
+}
