ec975cf1db0e1c553baf636501b81ee313e40971
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2022-12-24 20:20:23 -0300

    Feat(fields): add MaskedNumberField

---
+++ src/components/fields/MaskedNumberField/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { TextInputProps, Labeled, useRecordContext, useResourceContext, useTranslate } from 'react-admin'
+
+import { InputAttributes, NumericFormat, NumericFormatProps } from 'react-number-format'
+import { LabeledFieldProps } from '../../../config'
+import { MAPPED_CURRENCIES } from '../../../config/common/currencies'
+
+interface MaskedNumberFieldProps extends TextInputProps {
+  useCurrecyPrefix?: boolean
+}
+
+interface CustomProps {
+  name: string
+  prefix?: string
+  value: any
+}
+
+function ReadOnlyNumericFormat(props: any) {
+  return <span>{props.value}</span>
+}
+
+const NumericFormatCustom = React.forwardRef<NumericFormatProps<InputAttributes>, CustomProps>(
+  function NumericFormatCustom(props, ref) {
+    return (
+      <NumericFormat
+        {...props}
+        getInputRef={ref}
+        thousandSeparator
+        isNumericString
+        fixedDecimalScale
+        decimalScale={2}
+        readOnly
+        customInput={ReadOnlyNumericFormat}
+      />
+    )
+  },
+)
+
+export function MaskedNumberField(props: LabeledFieldProps<MaskedNumberFieldProps>) {
+  const { prefix, useLabel, label, useCurrecyPrefix } = props
+
+  const resource = useResourceContext()
+  const translate = useTranslate()
+
+  const record = useRecordContext(props)
+  const value = record[props.source]
+
+  const labelToUse = translate(label ? (label as string) : `resources.${resource}.fields.${props.source}`)
+
+  const useLabeledComponent = useLabel && label !== false
+
+  let prefixToUse = prefix
+  if (useCurrecyPrefix) prefixToUse = MAPPED_CURRENCIES?.[record.currency]?.symbol || ''
+
+  const field = <NumericFormatCustom prefix={prefixToUse} value={value} name={props.source} />
+
+  return useLabeledComponent ? <Labeled label={labelToUse}>{field}</Labeled> : field
+}
