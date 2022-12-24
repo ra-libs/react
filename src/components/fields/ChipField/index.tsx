f14721037fc2a5536b33746e6d7deb9ab120c472
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2022-12-24 19:57:59 -0300

    feat(fields): add ChipField

---
+++ src/components/fields/ChipField/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Chip, Typography } from '@mui/material'
+import { ChipFieldProps, useRecordContext, useResourceDefinition, useTranslate } from 'react-admin'
+
+export function ChipField(props: ChipFieldProps) {
+  const { className, source = 'id', emptyText } = props
+
+  const record = useRecordContext()
+  const translate = useTranslate()
+  let value = record?.[source]
+  const { recordRepresentation } = useResourceDefinition()
+
+  if (value == null && emptyText) {
+    return (
+      <Typography component='span' variant='body2' className={className}>
+        {emptyText && translate(emptyText, { _: emptyText })}
+      </Typography>
+    )
+  }
+
+  if (typeof recordRepresentation === 'string') value = record[recordRepresentation]
+  if (typeof recordRepresentation === 'function') value = recordRepresentation(record)
+  if (React.isValidElement(recordRepresentation)) return recordRepresentation
+
+  return <Chip label={value} sx={{ margin: '4px', cursor: 'inherit' }} />
+}
