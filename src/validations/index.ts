7c4a7933642857d0bed0b433d366779e94cf038a
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2023-01-18 12:26:42 -0300

    fix(validations): parse string to date in min,max year

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,11 +1,13 @@
 export const minYear =
     (year: number, message = 'validation.minYear') =>
         (value: Date) => {
-            return value.getFullYear() >= year ? undefined : { message, args: { year } };
+            const date = typeof value === "string" ? new Date(value) : value
+            return date?.getFullYear() >= year ? undefined : { message, args: { year } };
         }
 
 export const maxYear =
     (year: number, message = 'validation.maxYear') =>
-        (value: Date) => {
-            return value.getFullYear() <= year ? undefined : { message, args: { year } };
+        (value: Date | string) => {
+            const date = typeof value === "string" ? new Date(value) : value
+            return date.getFullYear() <= year ? undefined : { message, args: { year } };
         }
